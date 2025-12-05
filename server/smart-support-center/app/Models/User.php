56675d0379779588a7d001c0017a71db2fe778f2
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2025-12-05 08:59:05 +0200

    update user Model add identifier and get Jwt Cutom claims

--- server/smart-support-center/app/Models/User.php
+++ server/smart-support-center/app/Models/User.php
@@ -6,8 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -61,4 +62,14 @@ class User extends Authenticatable
     {
         return $this->role === 'admin';
     }
+
+    public function getJWTIdentifier()
+{
+    return $this->getKey();
+}
+
+public function getJWTCustomClaims()
+{
+    return [];
+}
 }
