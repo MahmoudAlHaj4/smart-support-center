7f5f7086325197f1ec48b597ca94c6e1c1794144
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2025-12-05 09:57:08 +0200

    add register  function

--- server/smart-support-center/app/Http/Controllers/AuthController.php
+++ server/smart-support-center/app/Http/Controllers/AuthController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Container\Attributes\Log;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        try{
+            $validate = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255|unique:users',
+            'password'=> 'required|string|min:8|confirmed',
+            'role' => 'customer',
+        ]);
+
+        if($validate->fails()){
+            return response()->json($validate->errors(), 422);
+        }
+
+        $user = User::create([
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password' => Hash::make($request->password),
+            'role'=> 'customer'
+        ]);
+        if (!$user) {
+            return response()->json(['error' => 'User creation failed'], 500);
+        }
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json([
+            'message'=>'User registered successfully',
+            'user' => $user,
+            'token'=>$token
+        ], 201);
+        }catch (\Exception $e) {
+        return response()->json([
+            'message' => 'Registration failed',
+            'error' => $e->getMessage()
+        ], 500);
+    }
+    }
 }
