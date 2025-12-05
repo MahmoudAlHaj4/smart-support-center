d40435059f4333ad82590b74851d896a6aedac67
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2025-12-05 10:40:11 +0200

    create login function

--- server/smart-support-center/app/Http/Controllers/AuthController.php
+++ server/smart-support-center/app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Container\Attributes\Log;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Symfony\Component\Mime\Message;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
@@ -44,6 +45,46 @@ class AuthController extends Controller
         }catch (\Exception $e) {
         return response()->json([
             'message' => 'Registration failed',
+            'error' => $e->getMessage()
+        ], 500);
+    }
+    }
+
+    public function login (Request $request) 
+    {
+        try{
+            $validate = Validator::make($request->all(),[
+                'email' =>'required|string|email',
+                'password'=>'required|string'
+            ]);
+            if($validate->fails()){
+                return response()->json($validate->errors(),422);
+            }
+            $user = User::where('email', $request->email)->first();
+
+            if(!$user){
+                return response()->json([
+                    'message' => 'Invalid Credentials'
+                ], 401);
+            }
+
+            if(!Hash::check($request->password, $user->password)){
+                return response()->json([
+                    'message'=> 'Invalid Credentials',
+                ]);
+
+            }
+
+            $token = JWTAuth::fromUser($user);
+
+            return response()->json([
+                'message'=>'Login success',
+                'user' =>$user,
+                'token'=>$token
+            ]);
+        }catch (\Exception $e) {
+        return response()->json([
+            'message' => 'Login failed',
             'error' => $e->getMessage()
         ], 500);
     }
