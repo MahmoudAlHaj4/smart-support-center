c974baf375ba0fb2364d7a05a84ae6155834b75a
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2025-12-10 12:42:00 +0200

    create Ticket Model

---
+++ server/smart-support-center/app/Models/Ticket.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'priority',
+        'category',
+    ];
+    // This fuction to get the customer who created this ticket 
+    public function customer()
+    {
+        return $this->belongsTo(User::class, 'customer_id');
+    }
+    // Get the agent assigned to this ticket
+    public function assignedAgent()
+    {
+        return $this->belongsTo(User::class, 'assigned_agent_id');
+    }
+}
+
