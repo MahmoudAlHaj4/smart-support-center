ed0aadda7bccd2b333b7e82bca82820fc51da44f
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2025-12-10 13:09:33 +0200

    create messages Model

---
+++ server/smart-support-center/app/Models/Message.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+   protected $fillable = [
+    'ticket_id',
+    'user_id', 
+    'sender_type',
+    'message'
+    ];
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
